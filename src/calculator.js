4ad03d9b24becf4a0e12d1740eef05ad5f639961
J <59581793+daechi73@users.noreply.github.com>
2023-05-10 18:36:25 -0400

    calculator subtract implemented

--- src/calculator.js
+++ src/calculator.js
@@ -2,8 +2,10 @@ const calculator = (a, b) => {
   const add = (a, b) => {
     return a + b;
   };
-
-  return { add };
+  const subtract = (a, b) => {
+    return a - b;
+  };
+  return { add, subtract };
 };
 
 export default calculator;
--- src/calculator.test.js
+++ src/calculator.test.js
@@ -3,3 +3,7 @@ import calculator from "./calculator";
 test("calculator add function", () => {
   expect(calculator().add(1, 2)).toBe(3);
 });
+
+test("Calculator subtract function testing...", () => {
+  expect(calculator().subtract(2, 1)).toBe(1);
+});
