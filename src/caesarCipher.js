3114efad12272b6f99e69fe8241a4773b313fbd4
J <59581793+daechi73@users.noreply.github.com>
2023-05-11 01:52:00 -0400

    caesarCipher Completed

--- src/caesarCipher.js
+++ src/caesarCipher.js
@@ -4,15 +4,38 @@ const caesarCipher = (string) => {
   const getCharCode = (char) => {
     return char.charCodeAt(0);
   };
+
   const shift = (shiftI) => {
+    if (shiftI > 26) shiftI = shiftI % 26;
+    if (shiftI < -26) shiftI = (shiftI % 26) * -1;
+
     for (let i = 0; i < string.length; i++) {
-      cipheredString += String.fromCharCode(
-        getCharCode(string.charAt(i)) + shiftI
-      );
+      let newCharCode;
+      const currentCharCode = getCharCode(string.charAt(i));
+      if (currentCharCode !== 32) {
+        newCharCode = currentCharCode + shiftI;
+      } else {
+        newCharCode = currentCharCode;
+      }
+
+      if (
+        (newCharCode >= 65 && newCharCode <= 90) ||
+        (newCharCode >= 97 && newCharCode <= 122) ||
+        newCharCode === 32
+      )
+        cipheredString += String.fromCharCode(newCharCode);
+      else if (newCharCode > 90 && currentCharCode <= 90) {
+        cipheredString += String.fromCharCode(newCharCode - 26);
+      } else if (newCharCode < 65 && currentCharCode <= 90) {
+        cipheredString += String.fromCharCode(26 + newCharCode);
+      } else if (newCharCode < 97 && currentCharCode >= 97) {
+        cipheredString += String.fromCharCode(26 + newCharCode);
+      } else if (newCharCode > 122 && currentCharCode <= 122) {
+        cipheredString += String.fromCharCode(newCharCode - 26);
+      } else return "shifting out of bounds from letter " + string.charAt(i);
     }
     return cipheredString;
   };
-
   return { shift };
 };
 
--- src/caesarCipher.test.js
+++ src/caesarCipher.test.js
@@ -1,6 +1,6 @@
 import caesarCipher from "./caesarCipher";
 
 test("testing CaesarCipher getCharCode...", () => {
-  const cipher = caesarCipher("A");
-  expect(cipher.shift(1)).toMatch(/B/);
+  const cipher = caesarCipher("wtf is going on over here ");
+  expect(cipher.shift(1)).toMatch(/abcdef/);
 });
